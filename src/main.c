0967cf36ee877d3e23b5d6511dcc56eb2332578c
Gilbert <galvarez6@miners.utep.edu>
2020-05-01 10:42:42 -0600

    lab2 buttons and sounds and screen changes color

--- src/main.c
+++ src/main.c
@@ -2,15 +2,19 @@
 #include "libTimer.h"
 #include "switches.h"
 #include "buzzer.h"
-#include "led.h"
+//#include "led.h"
+#include "lcdutils.h"
+#include "lcddraw.h"
 
 void main(void) 
 {  
   configureClocks();
   switch_init();
-  led_init();
+  //led_init();
   buzzer_init();
   enableWDTInterrupts();
+  lcd_init();
+  clearScreen(COLOR_BLACK);
 
   or_sr(0x18);  // CPU off, GIE on
 } 
